af52698a0b69e8d0760eaf540a8a065f1b2df409
Joseph Alcini <64275507+alcinija@users.noreply.github.com>
2021-07-22 23:21:54 -0400

    Add Drafting and Pick Changing to Draft.java

--- src/Draft.java
+++ src/Draft.java
@@ -6,15 +6,55 @@ public class Draft {
 	private boolean snake;
 	private int pickNo;
 	private int lastPickNo;
-	private int round;
+	private int currentRound;
+	private Team currentTeam;
 	
 	public Draft(ArrayList<Team> teams, int rounds, boolean snake) {
 		this.teams = teams;
 		this.rounds = rounds;
 		this.snake = snake;
 		this.pickNo = 1;
-		this.round = 1;
+		this.currentRound = 1;
 		this.lastPickNo = this.rounds * this.teams.size();
+		this.currentTeam = this.teams.get(0);
+	}
+	
+	public ArrayList<Team> getTeams() {
+		return this.teams;
+	}
+	
+	public int getRounds() {
+		return this.rounds;
+	}
+	
+	public boolean getSnake() {
+		return this.snake;
+	}
+	
+	public int getPickNo() {
+		return this.pickNo;
+	}
+	
+	public int getCurrentRound() {
+		return this.currentRound;
+	}
+	
+	public int getLastPickNo() {
+		return this.lastPickNo;
+	}
+	
+	public Team getCurrentTeam() {
+		return this.currentTeam;
+	}
+	
+	public boolean draftPlayer(Object[] newPlayer) {
+		if (this.snake && this.currentRound % 2 == 0) {
+			this.currentTeam.addPlayerToRoster(Integer.parseInt(newPlayer[0].toString()), Integer.parseInt(newPlayer[1].toString()), newPlayer[2].toString(), newPlayer[3].toString(), newPlayer[4].toString(), newPlayer[5].toString(), Integer.parseInt(newPlayer[6].toString()));
+			return true;
+		} else {
+			this.currentTeam.addPlayerToRoster(Integer.parseInt(newPlayer[0].toString()), Integer.parseInt(newPlayer[1].toString()), newPlayer[2].toString(), newPlayer[3].toString(), newPlayer[4].toString(), newPlayer[5].toString(), Integer.parseInt(newPlayer[6].toString()));
+			return true;
+		}
 	}
 	
 	public int nextPick() {
@@ -24,22 +64,38 @@ public class Draft {
 		
 		this.pickNo++;
 		
+		if (this.snake && this.currentRound % 2 == 0) {
+			this.currentTeam = this.teams.get(this.teams.size() - (this.pickNo % this.teams.size()) - 1);
+		} else {
+			this.currentTeam =  this.teams.get(this.pickNo % this.teams.size() - 1);
+		}
+		
+		if (this.pickNo % 10 == 0) {
+			this.currentRound++;
+		}
+		
 		return this.pickNo;
 	}
 	
-	public Team nextTeam() {
-		if (this.snake && this.round % 2 == 0) {
-			return this.teams.get(this.teams.size() - (this.pickNo % this.teams.size()));
-		} else {
-			return this.teams.get(this.pickNo % this.teams.size());
-		}
-	}
-	
-	public int nextPickRound() {
-		if (this.pickNo % 10 == 0) {
-			this.round++;
+	public int previousPick() {
+		if (this.pickNo < 1) {
+			return -1;
 		}
 		
-		return this.round;
+		this.currentTeam.removeLastPlayerFromRoster();
+		
+		this.pickNo--;
+		
+		if (this.snake && this.currentRound % 2 == 0) {
+			this.currentTeam = this.teams.get(this.teams.size() - (this.pickNo % this.teams.size()) - 1);
+		} else {
+			this.currentTeam =  this.teams.get(this.pickNo % this.teams.size() - 1);
+		}
+		
+		if (this.pickNo % 10 == 0) {
+			this.currentRound--;
+		}
+		
+		return this.pickNo;
 	}
 }
