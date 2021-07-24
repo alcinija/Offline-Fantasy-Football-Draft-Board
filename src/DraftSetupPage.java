e4f004dc0ab2c212d74ca9dd0458320a6dfcb849
Joseph Alcini <64275507+alcinija@users.noreply.github.com>
2021-07-23 23:52:10 -0400

    Add the Setup Page

---
+++ src/DraftSetupPage.java
@@ -0,0 +1,24 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+public class DraftSetupPage extends JFrame implements ActionListener {
+	private JComboBox year;
+	private JTextField roundsTF, numTeamsTF, teamNameTF;
+	private JButton ppr;
+	
+	public DraftSetupPage() {
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
