2a050aabab62bad5223f7a2f2902f6d555b35982
Joseph Alcini <64275507+alcinija@users.noreply.github.com>
2021-08-01 23:36:55 -0400

    Add files via upload

---
+++ src/Selection.java
@@ -0,0 +1,32 @@
+
+public class Selection {
+	private String teamName;
+	private Player player;
+	
+	public Selection(String teamName) {
+		this.teamName = teamName;
+		this.player = null;
+	}
+	
+	public void addPlayer(Player newPlayer) {
+		this.player = newPlayer;
+	}
+	
+	public void removePlayer() {
+		this.player = null;
+	}
+	
+	public Object[] getSelection() {
+		Object[] selection = new Object[2];
+		
+		selection[0] = this.teamName;
+		
+		if (this.player != null) {
+			selection[1] = this.player.toString();
+		} else {
+			selection[1] = "";
+		}
+		
+		return selection;
+	}
+}
