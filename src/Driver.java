adc56d83f953b5c84a69d899d8bc6dc6b446b3b9
Joseph Alcini <64275507+alcinija@users.noreply.github.com>
2021-08-07 23:21:13 -0400

    Add documentation

--- src/Driver.java
+++ src/Driver.java
@@ -1,10 +1,16 @@
 import javax.swing.WindowConstants;
 
+/**
+ * Name: Joe Alcini
+ * File: Driver.java
+ * Date: 8/05/2000
+ * Current Version: 1.0.0 Original Release
+ * Description: Starts running the application on execution
+ */
 public class Driver {
-
+	// Main method
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+		// Loads the setup page		
 		DraftSetupPage dsp =  new DraftSetupPage();
 		dsp.setSize(500, 300);
 		dsp.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
