dde056cab731e6e8434b97c6d92625cd688a0464
Joseph Alcini <64275507+alcinija@users.noreply.github.com>
2021-08-13 23:37:31 -0400

    Add files via upload

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,13 @@
+/**
+ * Name: Joe Alcini
+ * File: Driver.java
+ * Date: 8/05/2000
+ * Current Version: 1.0.0 Original Release
+ * Description: Creates a player object with data
+ * that can accessed
+ */
 public class Player implements Comparable<Player> {
+	// Declare fields
 	private int posRank;
 	private int ovlRank;
 	private String fName;
@@ -8,6 +17,16 @@ public class Player implements Comparable<Player> {
 	private int byeWeek;
 	private boolean drafted;
 	
+	/**
+	 * Creates a player object
+	 * @param posRank int The rank for the player at their position
+	 * @param ovlRank int The rank for the player overall
+	 * @param fName String The first name of the player
+	 * @param lName String The last name of the player
+	 * @param position String The position that the player plays
+	 * @param nflTeam String The NFL team that the player is on
+	 * @param byeWeek int The week that the player does not play
+	 */
 	public Player(int posRank, int ovlRank, String fName, String lName, String position, String nflTeam, int byeWeek) {
 		this.posRank = posRank;
 		this.ovlRank = ovlRank;
@@ -19,46 +38,91 @@ public class Player implements Comparable<Player> {
 		this.drafted = false;
 	}
 	
+	/**
+	 * Gets the Position rank of the player
+	 * @return The position rank of the player
+	 */
 	public int getPosrank() {
 		return this.posRank;
 	}
 	
+	/**
+	 * Gets the overall rank of the player
+	 * @return The overall rank
+	 */
 	public int getOvlrank() {
 		return this.ovlRank;
 	}
 	
+	/**
+	 * Gets the player's first name
+	 * @return The first name
+	 */
 	public String getFName() {
 		return this.fName;
 	}
 	
+	/**
+	 * Gets the player's last name
+	 * @return The player's last name
+	 */
 	public String getLName() {
 		return this.lName;
 	}
 	
+	/**
+	 * Returns a formated full name of the player
+	 * @return The full name
+	 */
 	public String getFullName() {
 		return this.fName + " " + this.lName;
 	}
 	
+	/**
+	 * Returns the position that the player plays
+	 * @return The position name
+	 */
 	public String getPosition() {
 		return this.position;
 	}
 	
+	/**
+	 * Returns the NFL team that the player plays for
+	 * @return The NFL team name
+	 */
 	public String getNFLTeam() {
 		return this.nflTeam;
 	}
 	
+	/**
+	 * Returns the week number the player does not play
+	 * @return The bye week number
+	 */
 	public int getByeWeek() {
 		return this.byeWeek;
 	}
 	
+	/**
+	 * Returns weather or not the player was drafted in the draft
+	 * @return The draft status
+	 */
 	public boolean getDrafted() {
 		return this.drafted;
 	}
 	
+	/**
+	 * Returns a formatted String with information
+	 * about the player
+	 * @return The formatted String
+	 */
+	@Override
 	public String toString() {
 		return this.fName + "  " + this.lName + "  " + this.position + "  " + this.nflTeam + "  " + this.byeWeek;
 	}
 	
+	/**
+	 * Changes the draft status of a player
+	 */
 	public void changeDrafted() {
 		if (this.drafted) {
 			this.drafted = false;
@@ -67,6 +131,11 @@ public class Player implements Comparable<Player> {
 		}
 	}
 
+	/**
+	 * Compares two player objects based on their ranks
+	 * @param otherPlayer Player the other player bing compared
+	 * @return The result of comparing two players
+	 */
 	@Override
 	public int compareTo(Player otherPlayer) {
 		if (this.getOvlrank() < otherPlayer.getOvlrank()) {
